804a3c63de11ecaca6f175ebfeeeaff33af34a0b
Kimari <grgkimari@gmail.com>
2022-09-06 19:31:54 +0300

    first commit

---
+++ src/components/QuoteBox.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { ReactDOM } from 'react'
+
+class QuoteBox extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {}
+    }
+    render(){
+        return ( 
+            <h1>QuoteBox</h1>
+        )}  
+}
+
+export default QuoteBox
