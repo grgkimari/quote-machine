6eabeb54fe2bd94fd7c887459f411ff7f9540246
Kimari <grgkimari@gmail.com>
2022-09-07 03:10:50 +0300

    created components, fetch quotes and new Quote fxnality

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,14 +1,54 @@
 import React from 'react'
-import { ReactDOM } from 'react'
+import Text from './Text'
+import Author from './Author'
+import NewQuote from './NewQuote'
+import TweetQuote from './TweetQuote'
 
 class QuoteBox extends React.Component{
     constructor(props){
         super(props)
-        this.state = {}
+        this.state = {
+            
+        }
+        this.getNewQuote = this.getNewQuote.bind(this)
     }
+
+
+    componentDidMount(){
+
+        this.setState({
+            data : [{
+                text : "Loading data ...",
+                author : "None"
+            }]
+        })
+
+        let url= "https://type.fit/api/quotes"
+        fetch(url)
+            .then((response) => response.json()).then((data) => {
+                this.setState({
+                    data: data,
+                })
+            })
+    }
+
     render(){
+        let obj = this.state.data
+        let text, author
+        
+        if(Array.isArray(obj)){
+            let index = Math.floor(Math.random() * obj.length)
+            text = obj[index].text
+            author= obj[index].author
+        }
         return ( 
-            <h1>QuoteBox</h1>
+            <div id="quote-box" className="container border border-5 text-center">
+                
+                <Text data ={text} />
+                <Author data = {author === null ? "Anonymous" : author}/>
+                <NewQuote  />
+                <TweetQuote/>
+            </div>
         )}  
 }
 
